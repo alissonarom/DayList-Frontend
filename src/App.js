a07ba1e08f38478e71d78c1055b1f7a03a2096d1
Alisson Morais <alissonmorais.br@gmail.com>
2021-07-28 15:11:53 -0300

    Design Cards and init API with Axios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,55 @@
-import logo from './logo.svg';
-import Header from './Header';
+import React, { useState } from 'react';
+
+import api from './Services/api';
+
+import './app.css';
+import './global.css';
+import './sidebar.css';
+import './main.css';
+
+import Notes from './Components/Notes';
+
+
 function App() {
+  const [title, setTitle] = useState('');
+  const [notes, setNotes] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+  }
+
   return (
-    <Header title='Meu título' />
+    <div id="app">
+      <aside>
+        <strong>Caderno de notas</strong>
+        <form onSubmit={handleSubmit} >
+
+          <div className="input-block">
+            <label htmlFor="title">Título da Anotação</label>
+            <input
+              required
+              value={title}
+            />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="nota">Anotações</label>
+            <textarea
+              required
+              value={notes}
+            />
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+        <ul>
+          <Notes />
+        </ul>
+      </main>
+    </div>
   );
 }
 
---
+++ src/Components/Notes/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function Notes() {
+  return (
+    <>
+      <li className="notepad-infos">
+        <div>
+          <strong>Fazer compras</strong>
+          <div>
+            x
+          </div>
+        </div>
+        <textarea>dolor sit amet, consectetur, adipisci velit...</textarea>
+        <span>!</span>
+      </li>
+      <li className="notepad-infos">
+        <div>
+          <strong>Estudar FullStack</strong>
+          <div>
+            x
+          </div>
+        </div>
+        <textarea>ctetur, adipisci velit...</textarea>
+        <span>!</span>
+      </li>
+    </>
+  )
+}
+
+export default Notes;
